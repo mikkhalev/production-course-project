784c3d747ab895379db50d06b61456bc1d7ec5a0
mikkhalev <a.mikkhalev@list.ru>
2025-03-02 19:16:15 +0300

    upgrade sidebar, add button themes

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -12,19 +12,7 @@ export const Navbar = ({ className }: NavbarProps) => {
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
             <div className={cls.links}>
-                <AppLink
-                    to="/"
-                    className={cls.mainLink}
-                    theme={AppLinkTheme.SECONDARY}
-                >
-                    {t('Главная страница')}
-                </AppLink>
-                <AppLink
-                    to="/about"
-                    theme={AppLinkTheme.SECONDARY}
-                >
-                    {t('О сайте')}
-                </AppLink>
+
             </div>
         </div>
     );
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,7 +3,12 @@ import React, { useState } from 'react';
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
 import { LangSwitcher } from 'widgets/LangSwitcher/LangSwitcher';
 import cls from './Sidebar.module.scss';
-import { BugButton } from "app/providers/ErrorBoundary";
+import { Button, ButtonSize, ButtonTheme } from "shared/ui/Button/Button";
+import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
+import { t } from "i18next";
+import { RoutePath } from "shared/config/routeConfig/routeConfig";
+import AboutIcon from 'shared/assets/icons/about.svg'
+import HomeIcon from 'shared/assets/icons/home.svg'
 
 interface SidebarProps {
   className?: string;
@@ -19,11 +24,41 @@ export const Sidebar = ({ className }: SidebarProps) => {
         <div
             className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}
         >
-            <button
+            <Button
                 onClick={onToggle}
+                className={cls.collapseBtn}
+                theme={ButtonTheme.BACKGROUND_INVERTED}
+                size={ButtonSize.L}
+                square
             >
-                toggle
-            </button>
+                {collapsed ? '>' : '<'}
+            </Button>
+            <div className={cls.items}>
+                <div className={cls.item}>
+                    <AppLink
+                        to={RoutePath.main}
+                        className={cls.mainLink}
+                        theme={AppLinkTheme.SECONDARY}
+                    >
+                        <HomeIcon className={cls.icon}/>
+                        <span>
+                            {t('Главная страница')}
+                        </span>
+                    </AppLink>
+                </div>
+                <div className={cls.item}>
+                    <AppLink
+                        to={RoutePath.about}
+                        className={cls.mainLink}
+                        theme={AppLinkTheme.SECONDARY}
+                    >
+                        <AboutIcon className={cls.icon}/>
+                        <span>
+                            {t('О сайте')}
+                        </span>
+                    </AppLink>
+                </div>
+            </div>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
                 <LangSwitcher className={cls.lang} />
